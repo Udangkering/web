da97456cbac510dd11427b80c87b4178cf57f3fe
Udangkering <88819514+Udangkering@users.noreply.github.com>
2022-10-26 10:09:23 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Personal
+* Web yang berisi berbagai macam link sosmed, dll
+* Contoh Tampilannya? <a href="//jauhariq.github.io/jauhhari/">Klik disini</a>
---
+++ script.js
@@ -0,0 +1,2 @@
+function play() {
+var audio = new Audio('https://www.mboxdrive.com/y2mate.com%20-%20JVKE%20i%20cant%20help%20it%20lyric%20video%20(mp3cut.net).mp3');audio.play();}
