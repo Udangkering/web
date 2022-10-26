dcc9fe493a3ba5b20b2c5a73176839a808ce9b8c
Udangkering <88819514+Udangkering@users.noreply.github.com>
2022-10-26 10:58:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 function play() {
 var audio = new Audio('https://www.mboxdrive.com/y2mate.com%20-%20JVKE%20i%20cant%20help%20it%20lyric%20video%20(mp3cut.net).mp3');audio.play();}
+
+function showFt() {document.getElementById('foto').style.display = "block";}
+function showDiv() {document.getElementById('Content').style.display = "block";}
